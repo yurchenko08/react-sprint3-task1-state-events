d1f366c9baa8df276d8b01255b0c8919e2128aaf
yurchenko08 <yurchenko08@gmail.com>
2022-10-26 19:33:58 +0300

    finished task3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { findRenderedDOMComponentWithClass } from "react-dom/test-utils";
+import "./App.css";
 
 export default class App extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = { appData: "React Marathon" };
+    this.onClickHandler = this.onClickHandler.bind(this);
+  }
+  onClickHandler = () =>
+    this.setState({
+      appData: this.state.appData.toLowerCase(),
+    });
   render() {
-    return <div> React Marathon </div>    
+    return <div onClick={this.onClickHandler}> {this.state.appData}</div>;
   }
 }
